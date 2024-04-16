4f039ee92a2251daf103d110cdcf8d28156f8914
coastalwhite <me@gburghoorn.com>
2024-04-16 14:27:41 +0200

    Update Nix flake to include correct targets

--- flake.nix
+++ flake.nix
@@ -2,34 +2,46 @@
   description = "A very basic flake";
 
   inputs = {
-      nixpkgs.url = "github:NixOS/nixpkgs";
-      flake-utils.url = "github:numtide/flake-utils";
-
-      riscv-tests.url = "github:coastalwhite/riscv-tests-nixflake";
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    riscv-tests.url = "github:coastalwhite/riscv-tests-nixflake";
   };
 
-  outputs = { self, nixpkgs, flake-utils, riscv-tests }:
-    flake-utils.lib.eachDefaultSystem (system: let
-        pkgs = import nixpkgs { inherit system; };
-        riscv-toolchain = import nixpkgs {            
-            localSystem = system;
-            crossSystem = {
-                config = "riscv32-none-elf";
-            };
-        };
-        in rec {
-            devShells.default = devShells.riscv32-binutils;
-            devShells.riscv32-binutils = pkgs.mkShell {
-                packages = [
-                    riscv-toolchain.buildPackages.gcc
-                ];
+  outputs = { self, nixpkgs, utils, rust-overlay, riscv-tests }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ rust-overlay.overlays.default ];
+      };
 
-				shellHook = ''
-                    export RISCV_TESTS="${riscv-tests.packages.${system}.default}"
-                    export RISCV="${riscv-toolchain.buildPackages.gcc}"
-					rustup target add riscv32i-unknown-none-elf
-					rustup target add riscv32imac-unknown-none-elf
-				'';
-			};
-        });
+      rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+        targets = [
+          "riscv32i-unknown-none-elf"
+          "riscv32imac-unknown-none-elf"
+          "x86_64-unknown-linux-gnu"
+        ];
+      };
+
+      riscv-toolchain = import nixpkgs {            
+        localSystem = system;
+        crossSystem = {
+            config = "riscv32-none-elf";
+        };
+      };
+      in rec {
+        devShells.default = devShells.riscv32-binutils;
+        devShells.riscv32-binutils = pkgs.mkShell {
+          packages = [
+            riscv-toolchain.buildPackages.gcc
+            rustToolchain
+          ];
+
+			    shellHook = ''
+            export RISCV_TESTS="${riscv-tests.packages.${system}.default}"
+            export RISCV="${riscv-toolchain.buildPackages.gcc}"
+			    '';
+			  };
+      }
+    );
 }
